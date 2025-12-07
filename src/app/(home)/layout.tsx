d9db0696d286b4f0946fcde1867dff27977b4390
Chetan Sonawane <chetansonawane2006@gmail.com>
2025-12-07 21:30:51 +0530

    Logo Uploaded

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "@/modules/home/ui/components/navbar";
 import Footer from "@/components/Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  
   return (
     <main className="relative flex flex-col min-h-screen w-full overflow-hidden">
       <Navbar />
